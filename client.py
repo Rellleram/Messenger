ce5f6c05981bc8537020e3a29433d3f0ee2a3526
Rellleram <dragon.midav2@gmail.com>
2025-10-07 17:27:23 +0300

    Написал функционал отправки сообщения клиентом, принятия этого сообщение от клиента и рассылка его через функцию broadcast остальным подключенным клиентом. Этакий групповой чат. Реализованы две команды на сервер /shutdown и /list для выключения сервера и просмотра подключенных клиентов соответственно. На сервере реализовано два потока - один постоянно ожидает подключения клиентов и их сообщений, второй ожидает ввода доступных команд. На клиенте реализован поток, который постоянно ждет сообщений от других клиентов и через цикл реализована возможность отправки сообщения. Если клиент больше не хочет общаться - есть команда /exit которая завершает работу клиента отправляя на сервер запрос __disconnect__ который отключает этого клиента и чистит его в словаре. Пока проблема которую я заметил - если клиент в процессе написания никнейма, а сервер завершил работу, клиент все еще может написатьникнейм и еще сообщение, хотя сервер уже не работает. В следующем коммите постараюсь исправить.

--- client.py
+++ client.py
@@ -1,13 +1,50 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
 PORT = 9090
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((HOST, PORT))
 
-client.send('Hello from client!'.encode('utf-8'))
+stop_flag = threading.Event()
 
-print(f'Response: {client.recv(1024).decode('utf-8')}')
+def rcv_msg():
+    while not stop_flag.is_set():
+        try:
+            msg = client.recv(1024).decode('utf-8')
+            if not msg:
+                print('Server closed connection')
+                stop_flag.set()
+                break
+            print(msg)
+        except OSError:
+            break
+        except Exception as e:
+            print(f'Error receiving message: {e}')
+            break
+
+try:
+    client.connect((HOST, PORT))
+    print('Connected to server')
+except ConnectionRefusedError:
+    print('Connection failed')
+    exit()
+
+print(client.recv(1024).decode('utf-8'))
+nickname = input()
+client.send(nickname.encode('utf-8'))
+
+threading.Thread(target=rcv_msg, daemon=True).start()
+
+while not stop_flag.is_set():
+    msg = input('Введите сообщение. Для выхода введите /exit\n')
+    if msg == '/exit':
+        client.send('__disconnect__'.encode('utf-8'))
+        stop_flag.set()
+        break
+    try:
+        client.send(msg.encode('utf-8'))
+    except:
+        print('Не удалось отправить сообщение. Сервер недоступен.')
 
 client.close()
--- server.py
+++ server.py
@@ -1,23 +1,79 @@
 import socket
+import threading
+from multiprocessing import Value
 
 HOST = '127.0.0.1'
 PORT = 9090
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
-server.listen(1)
+server.listen()
 
-print('Server started, waiting for connection...')
+clients = {}
 
-cl_sckt, addr = server.accept()
 
-print('New connection!', addr)
+def broadcast(client_socket, nick, message):
+    for client in clients.values():
+        if client != client_socket:
+            client.send(f'[{nick}]: {message.decode("utf-8")}'.encode('utf-8'))
 
-msg = cl_sckt.recv(1024).decode('utf-8')
+def server_commands(shutdown):
+    while True:
+        command = input()
+        if command == '/list':
+            print(f'Список клиентов:\n{clients}')
+        elif command == '/shutdown':
+            shutdown.value = True
+            print("Server shutting down...")
+            break
+        else:
+            print('Unknown command')
 
-print('Message from client:', msg)
+def msg_handler(client, nickname, shutdown):
+    while not shutdown.value:
+        try:
+            msg = client.recv(1024)
+            if not msg:
+                break   
+            elif msg.decode('utf-8') == '__disconnect__':
+                break
+            broadcast(client, nickname, msg)   
+        except Exception as e:
+            print(f"Error receiving message: {e}")
+            break
+    with threading.Lock():
+        print(f'Client {nickname} disconnected')
+        del clients[nickname]
+    client.close()
+    print(f'Client {nickname} cleaned up')   
 
-cl_sckt.send('Hello from server'.encode('utf-8'))
+def receive():
+    print('Server started')
+    print('Available commands: /list, /shutdown')
+    shutdown = Value('b', False)
+    thread1 = threading.Thread(target=server_commands, args = (shutdown, ))
+    thread1.start()
+    server.settimeout(1.0)
+    while not shutdown.value:
+        try:
+            cl_sckt, addr = server.accept()
+            print(f'Client {addr} connected')
+            cl_sckt.send('Вы подключились к серверу! Введите ваш никнейм'.encode('utf-8'))
 
-cl_sckt.close()
-server.close()
+            cl_nickname = cl_sckt.recv(1024).decode('utf-8')
+            print(f'Client with address {addr} set nickname:', cl_nickname)
+            clients[cl_nickname] = cl_sckt
+
+            thread2 = threading.Thread(target=msg_handler, args=(cl_sckt, cl_nickname, shutdown))    
+            thread2.start()
+        except socket.timeout:
+              continue
+        except Exception as e:
+            if shutdown.value:
+                break
+            print(f"Error accepting connection: {e}")
+
+    print('Closing server...')
+    server.close()
+
+receive()
