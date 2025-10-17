a8b8392f69b8de4a6461f44127474427cbba89c6
Rellleram <dragon.midav2@gmail.com>
2025-10-17 09:42:37 +0300

    Добавил комментарии

--- new_client.py
+++ new_client.py
@@ -1,9 +1,11 @@
 import asyncio
 
-
+#Объявляем IP-адрес и порт по которым клиент будет подключаться к серверу.
 HOST = '127.0.0.1'
 PORT = 9090
 
+'''Функция отправки сообщений. Запускается бесконечный цикл ожидания ввода сообщения пользователем и отправки его серверу.
+Если пользователь вводит /exit, то серверу отправляется сообщение, что клиент отключился и цикл завершается.'''
 async def send_message(writer):
     print('Введите сообщение. Для выхода введите /exit')
     loop = asyncio.get_running_loop()
@@ -16,6 +18,8 @@ async def send_message(writer):
         writer.write((msg + '\n').encode())
         await writer.drain()
 
+'''Функция приема сообщений. Бесконечный цикл ожидания сообщений от сервера и их вывода. 
+Если сервер принудительно завершает работу, цикл завершается.'''
 async def receive_message(reader):
     while True:
         msg = (await reader.readline()).decode().strip()
@@ -25,7 +29,8 @@ async def receive_message(reader):
             break
         print(msg)
 
-
+'''Основная функция. Запускается функция подключения к серверу и выводится приветственное сообщение.
+Пользователю предлагается ввести никнейм и после этого начинается отправка и прием сообщений.'''
 async def main():
     reader, writer = await asyncio.open_connection(HOST, PORT)
     print((await reader.readline()).decode().strip())
@@ -35,6 +40,7 @@ async def main():
     await writer.drain()
     rcv_msg = asyncio.create_task(receive_message(reader))
     snd_msg = asyncio.create_task(send_message(writer))
+    #Ожидание завершение хотя бы одной из задач. Если одна из них завершилась, то завершается и вторая.
     await asyncio.wait([rcv_msg, snd_msg], return_when=asyncio.FIRST_COMPLETED)
     rcv_msg.cancel()
     snd_msg.cancel()
--- new_server.py
+++ new_server.py
@@ -1,18 +1,25 @@
 import asyncio
 from datetime import datetime
 
+#Объявляем IP-адрес и порт на котором будет работать сервер.
 HOST = '127.0.0.1'
 PORT = 9090
 
+'''Создаем список клиентов, объявляем экземпляр класса Lock для предотвращения конфликтов при множественном доступе к словарю.
+Объявлем множество для хранения задач клиентов, чтобы впоследствии их корректно завершать через цикл.'''
 clients = {}
 clients_lock  = asyncio.Lock()
 clients_tasks = set()
 
+'''Функция для обработки команд сервера. В данный момент доступны две команды - /shutdown и /list.
+Первая команда завершает работу сервера, последовательно отключая всех клиентов и завершая их задачи.
+Вторая команда выводит список подключенных клиентов.'''
 async def server_commands(server):
     loop = asyncio.get_running_loop()
     while True:
         command = await loop.run_in_executor(None, input)
         if command == '/shutdown':
+            #Делаем блокировку, создаем список из значений словаря и потом его очищаем. Затем проходим по всем клиентам и отправляем им сообщение о завершении работы.
             async with clients_lock:
                 targets = list(clients.values())
                 clients.clear()
@@ -42,17 +49,24 @@ async def server_commands(server):
         else:
             print('Unknown command')
 
+'''Функция для отправки сообщений всем клиентам. Принимает клиента, его никнейм и текст сообщения.
+Через цикл проходимся по всем клиентам, кроме того, который отправил сообщение и отправляем текст сообщения'''
 async def broadcast(client, nickname, message):
     for clnt in clients.values():
         if clnt != client:
             clnt.write(f'{datetime.now().strftime("%H:%M")} [{nickname}]: {message}\n'.encode())
             await clnt.drain()
 
+'''Фунция принятия и обработки клиентов. При каждом новом подключении создается задача.
+При подключении клиенту отправляется сообщением о том, что он подключился к серверу и предлагается ввести никнейм. Пока клиент не введет никнейм общаться нельзя.
+Если никнейм, который клиент ввел уже существует - ему об этом сообщается и он должен ввести другой никнейм.
+Дальше бесконечный цикл ожидания сообщения от клиента.'''
 async def handle_client(reader, writer):
     task = asyncio.current_task()
     clients_tasks.add(task)
     
     try:
+        #Получаем IP-адрес и порт клиента
         addr = writer.get_extra_info('peername')
         print(f'Client {addr} connected')
         writer.write('Вы подключились к серверу!\n'.encode())
@@ -78,11 +92,14 @@ async def handle_client(reader, writer):
                 if not data:
                     break
                 msg = data.decode().strip()
+                #Пользователь может захотеть сам отключиться, в таком случае в клиенте реализована команда /exit(смотри код клиента).
+                #При таком отключении сервер всем отправляет, что пользователь вышел из чата, удаляет клиента из словаря и закрывает его соединение.
                 if msg == '__disconnect__':
                     await broadcast(writer, nickname, 'Пользователь вышел из чата.')
                     async with clients_lock:
                         del clients[nickname]
                     break
+                #Отправляем сообщения клиента всем.
                 await broadcast(writer, nickname, msg)
             except asyncio.CancelledError:
                 break
@@ -95,6 +112,8 @@ async def handle_client(reader, writer):
         clients_tasks.discard(task)
         print(f'Client {addr} disconnected')
         
+'''Основная функция. Запускается сервер. Запускается функция принятия и обработки клиентов. Запускается функция обработки команд сервера.
+Сервер работает бесконечно, пока не будет введена команда /shutdown'''
 async def main():
     server = await asyncio.start_server(handle_client, HOST, PORT)
     print('Server started. Available commands: /list, /shutdown')
